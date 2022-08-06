464462a018c546bfade889e99d7a434f99e2d233
cchalamon <cyril.chalamon@celaneo.com>
2022-08-06 13:11:47 +0200

    Force latests 10 posts if not found based on everwpflipboard metadata

--- everwpflipboard.php
+++ everwpflipboard.php
@@ -138,6 +138,12 @@ function flipBoardFeed() {
         'posts_per_page' => -1
     );
     $posts = get_posts($args);
+    if (!$posts || empty($posts)) {
+        $args = array(
+            'posts_per_page' => 10
+        );
+        $posts = get_posts($args);
+    }
     $flipBoardFile = 'flipboard.xml';
     $rss = '<rss xmlns:atom="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:creativeCommons="http://backend.userland.com/creativeCommonsRssModule" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:sy="http://purl.org/rss/1.0/modules/syndication/" version="2.0">
     <channel>
