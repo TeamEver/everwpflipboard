d8c445bb1f18a242f8be96f6f4fc9842529a638a
cchalamon <cyril.chalamon@celaneo.com>
2022-08-06 11:54:31 +0200

    Init plugin

---
+++ everwpflipboard.php
@@ -0,0 +1,14 @@
+<?php
+/* @wordpress-plugin
+ * Plugin Name:       Flipboard RSS plugin
+ * Plugin URI:        http://www.team-ever.com
+ * Description:       Flipboard RSS plugin for WordPress
+ * Version:           1.0.1
+ * Author:            Team Ever
+ * Author URI:        https://www.team-ever.com
+ * Text Domain:       everwpflipboard
+ * Domain Path: /languages
+ * License:           Tous droits réservés / Le droit d'auteur s'applique (All rights reserved / French copyright law applies)
+ * Copyright:       Cyril CHALAMON - Team Ever
+ * Author:       Cyril CHALAMON - Team Ever
+ */
