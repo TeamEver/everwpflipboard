2c3c6b1c28cb77fd16addbe1893a252ed6c46bed
cchalamon <cyril.chalamon@celaneo.com>
2022-09-03 11:06:37 +0200

    Fix XML

--- everwpflipboard.php
+++ everwpflipboard.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Flipboard RSS plugin
  * Plugin URI:        http://www.team-ever.com
  * Description:       Flipboard RSS plugin for WordPress
- * Version:           1.0.1
+ * Version:           1.0.2
  * Author:            Team Ever
  * Author URI:        https://www.team-ever.com
  * Text Domain:       everwpflipboard
@@ -30,14 +30,14 @@ function flipBoardFeed() {
         'post_status' => 'publish' // Show only the published posts
     ));
     $flipBoardFile = 'flipboard.xml';
-    $rss = '<rss xmlns:atom="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:creativeCommons="http://backend.userland.com/creativeCommonsRssModule" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:sy="http://purl.org/rss/1.0/modules/syndication/" version="2.0">
+    $rss = '<rss version="2.0">
     <channel>
     <title>'.$blogInfos['name'].'</title>
-    <link>'.$blogInfos['url'].'</link>
+    <link>'.$blogInfos['url'].'/flipboard.xml</link>
     <description>
     '.$blogInfos['description'].'
     </description>
-    <language>'.$blogInfos['language'].'</language>';
+    <language>'.strtolower($blogInfos['language']).'</language>';
     foreach ($posts as $post) {
         $author = get_user_by( 'id', $post['post_author'] );
         if (empty($post['post_excerpt'])) {
@@ -48,16 +48,17 @@ function flipBoardFeed() {
         $postThumbnail = get_the_post_thumbnail_url( $post['ID'], 'medium' );
         $post_categories = wp_get_post_categories( $post['ID'] );
         $defaultCategory = '';
-             
         foreach($post_categories as $c){
             $cat = get_category( $c );
             $defaultCategory .= $cat->name;
         }
+        $datetime = new DateTime($post['post_date_gmt']);
+        $postDate = $datetime->format('c');
         $rss .= '<item>
         <title>'.sanitize_text_field($post['post_title']).'</title>
         <link>'.get_permalink($post['ID']).'</link>
         <guid>'.get_permalink($post['ID']).'</guid>
-        <pubDate>'.sanitize_text_field($post['post_date_gmt']).'</pubDate>
+        <pubDate>'.sanitize_text_field($postDate).'</pubDate>
         <dc:creator xmlns:dc="creator">'.sanitize_text_field($author->display_name).'</dc:creator>
         <description><![CDATA[
         '.strip_tags(sanitize_text_field($postDescription)).'
