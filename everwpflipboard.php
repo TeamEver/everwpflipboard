9fb3c0424006ed193f34da2bf11d1978f99edd9c
cchalamon <cyril.chalamon@celaneo.com>
2022-12-19 18:05:15 +0100

    Set dom document

--- everwpflipboard.php
+++ everwpflipboard.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Flipboard RSS plugin
  * Plugin URI:        http://www.team-ever.com
  * Description:       Flipboard RSS plugin for WordPress
- * Version:           1.0.2
+ * Version:           2.0.1
  * Author:            Team Ever
  * Author URI:        https://www.team-ever.com
  * Text Domain:       everwpflipboard
@@ -16,9 +16,9 @@ if (!defined('ABSPATH')) {
   exit; // Exit if accessed directly
 }
 function flipBoardFeed() {
-    $everwpflipboard_qty = (int)get_option( 'everwpflipboard_qty' );
-    if ($everwpflipboard_qty <= 0) {
-        $everwpflipboard_qty = 50;
+    $everwpflipboardQty = (int)get_option( 'everwpflipboard_qty' );
+    if ($everwpflipboardQty <= 0) {
+        $everwpflipboardQty = 50;
     }
     $fields = array('name', 'description', 'wpurl', 'url', 'admin_email', 'charset', 'version', 'html_type', 'text_direction', 'language');
     $blogInfos = array();
@@ -26,18 +26,31 @@ function flipBoardFeed() {
         $blogInfos[$field] = get_bloginfo($field);
     }
     $posts = wp_get_recent_posts(array(
-        'numberposts' => $everwpflipboard_qty, // Number of recent posts thumbnails to display
+        'numberposts' => (int)$everwpflipboardQty, // Number of recent posts thumbnails to display
         'post_status' => 'publish' // Show only the published posts
     ));
     $flipBoardFile = 'flipboard.xml';
-    $rss = '<rss version="2.0">
-    <channel>
-    <title>'.$blogInfos['name'].'</title>
-    <link>'.$blogInfos['url'].'/flipboard.xml</link>
-    <description>
-    '.$blogInfos['description'].'
-    </description>
-    <language>'.strtolower($blogInfos['language']).'</language>';
+    $dom = new DOMDocument();
+    $dom->encoding = 'UTF-8';
+    $dom->xmlVersion = '1.0';
+    $dom->formatOutput = true;
+
+    // Add elements
+    $nodeDocument = $dom->createElement('channel');
+    $dom->appendChild($nodeDocument);
+
+    $titleNode = $dom->createElement('title', $blogInfos['name']);
+    $nodeDocument->appendChild($titleNode);
+
+    $linkNode = $dom->createElement('link', $blogInfos['url'].'/flipboard.xml');
+    $nodeDocument->appendChild($linkNode);
+
+    $descriptionNode = $dom->createElement('description', $blogInfos['name']);
+    $nodeDocument->appendChild($descriptionNode);
+
+    $languageNode = $dom->createElement('language', strtolower($blogInfos['language']));
+    $nodeDocument->appendChild($languageNode);
+
     foreach ($posts as $post) {
         $author = get_user_by( 'id', $post['post_author'] );
         if (empty($post['post_excerpt'])) {
@@ -50,32 +63,60 @@ function flipBoardFeed() {
         $defaultCategory = '';
         foreach($post_categories as $c){
             $cat = get_category( $c );
-            $defaultCategory .= $cat->name;
+            $defaultCategory = $cat->name;
         }
         $datetime = new DateTime($post['post_date_gmt']);
         $postDate = $datetime->format('c');
-        $rss .= '<item>
-        <title>'.sanitize_text_field($post['post_title']).'</title>
-        <link>'.get_permalink($post['ID']).'</link>
-        <guid>'.get_permalink($post['ID']).'</guid>
-        <pubDate>'.sanitize_text_field($postDate).'</pubDate>
-        <dc:creator xmlns:dc="creator">'.sanitize_text_field($author->display_name).'</dc:creator>
-        <description><![CDATA[
-        '.strip_tags(sanitize_text_field($postDescription)).'
-        ]]>
-        </description>';
+
+        $itemNode = $dom->createElement('item');
+        $nodeDocument->appendChild($itemNode);
+
+        $titlePostNode = $dom->createElement('title', sanitize_text_field($post['post_title']));
+        $itemNode->appendChild($titlePostNode);
+
+        $linkPostNode = $dom->createElement('link', get_permalink($post['ID']));
+        $itemNode->appendChild($linkPostNode);
+
+        $guidPostNode = $dom->createElement('guid', get_permalink($post['ID']));
+        $itemNode->appendChild($guidPostNode);
+
+        $pubDatePostNode = $dom->createElement('pubDate', sanitize_text_field($postDate));
+        $itemNode->appendChild($pubDatePostNode);
+
+        // $dcCreatorPostNode = $dom->createElement('dc:creator', sanitize_text_field($author->display_name));
+        $dcCreatorPostNode = $dom->createElement('dc', sanitize_text_field($author->display_name));
+        $itemNode->appendChild($dcCreatorPostNode);
+
+        $descriptionPostNode = $dom->createElement(
+            'description',
+            html_entity_decode(
+                strip_tags(
+                    sanitize_text_field($postDescription)
+                )
+            )
+        );
+        $itemNode->appendChild(
+            $descriptionPostNode
+        );
+
         if ($postThumbnail) {
-            $rss .= '<enclosure url="'.sanitize_url($postThumbnail).'" length="1000" type="image/jpeg" />';
+            $enclosurePostNode = $dom->createElement('enclosure');
+            $urlAttr = new DOMAttr('url', sanitize_url($postThumbnail));
+            $enclosurePostNode->setAttributeNode($urlAttr);
+            $lengthAttr = new DOMAttr('length', '1000');
+            $enclosurePostNode->setAttributeNode($lengthAttr);
+            $enclosurePostNode->setAttributeNode($urlAttr);
+            $typeAttr = new DOMAttr('type', 'image/jpeg');
+            $enclosurePostNode->setAttributeNode($typeAttr);
+            $itemNode->appendChild($enclosurePostNode);
         }
         if ($defaultCategory) {
-            $rss .= '<category>'.sanitize_text_field($defaultCategory).'</category>';
+            $categoryPostNode = $dom->createElement('category', sanitize_text_field($defaultCategory));
+            $itemNode->appendChild($categoryPostNode);
         }
-        $rss .= '</item>';
     }
-    $rss.= '</channel>
-    </rss>
-    ';
-    file_put_contents(ABSPATH . '/' .$flipBoardFile, $rss);
+
+    $dom->save(ABSPATH . '/' .$flipBoardFile);
 }
 /* 
  * Register settings 
@@ -104,8 +145,8 @@ function everwpflipboard_register_settings() {
  * Print settings field content 
  */
 function everwpflipboard_print_input_number() {
-    $everwpflipboard_qty = get_option( 'everwpflipboard_qty' );
-    echo '<input type="number" id="everwpflipboard_qty" name="everwpflipboard_qty" value="' . $everwpflipboard_qty . '" />';
+    $everwpflipboardQty = get_option( 'everwpflipboard_qty' );
+    echo '<input type="number" id="everwpflipboard_qty" name="everwpflipboard_qty" value="' . $everwpflipboardQty . '" />';
 }
 add_action( 'save_post', 'flipBoardFeed', 10, 3 );
 add_action( 'admin_init', 'everwpflipboard_register_settings' );
